04e9249875417f7a3727f88d12f63072f438b9cc
Raquel Câmara <raqclf@gmail.com>
2022-04-06 17:29:09 +0100

    SQL 2.05 activity

---
+++ lab-script.sql
@@ -0,0 +1,74 @@
+USE sakila;
+
+##Select all the actors with the first name ‘Scarlett’.
+SELECT first_name
+FROM sakila.actor
+WHERE first_name = 'Scarlett';
+
+##How many films (movies) are available for rent and how many films have been rented?
+SELECT COUNT(DISTINCT inventory_id) AS rentable_films ## We interpreted it as the number of copies of movie titles available to rent.
+FROM sakila.rental
+;
+SELECT COUNT(*)
+FROM sakila.rental
+WHERE rental_date IS NOT NULL AND rental_date NOT LIKE ' '; ## Copies of films that have been ever rented.
+
+##What are the shortest and longest movie duration? Name the values max_duration and min_duration.
+SELECT MIN(length) AS min_duration, MAX(length) AS max_duration
+FROM sakila.film
+HAVING min_duration = MIN(length) AND max_duration = MAX(length);
+
+##What's the average movie duration expressed in format (hours, minutes)?
+SELECT DATE_FORMAT(SEC_TO_TIME(AVG(length)*60), '%H:%i') AS avg_duration
+FROM sakila.film;
+
+##How many distinct (different) actors' last names are there?
+SELECT COUNT(DISTINCT last_name) AS lastnames_count
+FROM sakila.actor;
+
+##Since how many days has the company been operating (check DATEDIFF() function)?
+SELECT DATEDIFF(MAX(last_update),MIN(rental_date)) AS operation_days ## Last_update is considered the current date
+FROM sakila.rental;
+
+##Show rental info with additional columns month and weekday. Get 20 results.
+SELECT rental_date, DATE_FORMAT((rental_date), '%M') AS month
+	, DATE_FORMAT((rental_date), '%W') AS weekday 
+FROM sakila.rental
+ORDER BY rental_date
+LIMIT 20;
+
+##Add an additional column day_type with values 'weekend' and 'workday' depending on the rental day of the week.
+SELECT rental_date, DATE_FORMAT(rental_date, '%M') AS month
+	, DATE_FORMAT(rental_date, '%W') AS weekday
+    , CASE
+		WHEN DATE_FORMAT(rental_date, '%w') > 5 THEN 'weekend'
+        ELSE 'workday' 
+	END AS daytype
+FROM sakila.rental;
+
+##Get release years.
+SELECT title, release_year
+FROM sakila.film
+ORDER BY release_year;
+
+##Get all films with ARMAGEDDON in the title.
+SELECT title
+FROM sakila.film
+WHERE title LIKE '%ARMAGEDDON%';
+
+##Get all films which title ends with APOLLO.
+SELECT title
+FROM sakila.film
+WHERE title LIKE '%APOLLO';
+
+##Get 10 the longest films.
+SELECT title, length
+FROM sakila.film
+ORDER BY length DESC
+LIMIT 10;
+
+##How many films include Behind the Scenes content?
+
+SELECT COUNT(special_features)
+FROM sakila.film
+WHERE special_features LIKE '%Behind the Scenes%'
